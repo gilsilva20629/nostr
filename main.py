ac91a5f52797afc04973cd3ca4a8f75b1efe8a10
Gilberto <gilsilva20629@gmail.com>
2024-10-01 11:01:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,37 @@ import json
 
 from keyPair import KeyPair
 from event import Event
-from relayPool import RelayPool
+import message
+from filter import Filter
+#from relayPool import RelayPool
 
-relayPool = RelayPool()
-
+'''
 keyPair = KeyPair("b2776165d40acc349ad1f7f11105b037a3403971ee1be87787b47b407b6eb77f")
-
-user_data = json.dumps({ "name": "Dev Test", "displayName": "Hangle" })
-
+user_data = json.dumps({"name": "test_dev_nostr", "displayName": "GSO"})
 event = Event(kind=0, author=keyPair.getPubkey(), content=user_data)
 
-event.sign(keyPair)
-
-msg = f'["EVENT", {event.serialise()}]'
-
-def hello():
-	with connect("wss://relay.damus.io") as websocket:
-		websocket.send(msg)
-		message = websocket.recv()
-		print(f"Received: {message}")
+event.sign(keyPair)	
 
 
-hello()
+msg_event = message.MessageEvent()
+print(msg_event.sendMessage("wss://relay.damus.io", event.serialize()))
+'''
 
-relayPool.fetchEvents(keyPair.getPubkey(), 0, 1)
+authors = ["04c915daefee38317fa734444acee390a8269fe5810b2241e5e6dd343dfbecc9"]
+filter01 = Filter(authors,[1],3)
+
+authors = ["82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2"]	
+filter02 = Filter(authors,[1],3)
+
+authors = ["6e468422dfb74a5738702a8823b9b28168abab8655faacb6853cd0ee15deee93"]
+filter03 = Filter(authors,[1],3)
+
+msg_req = message.MessageReq()
+#print(msg_req.sendMessage("wss://relay.damus.io", "asdfasdasd", [filter01.build(), filter02.build(), filter03.build()]))
+#print(msg_req.sendMessage("wss://relay.damus.io", "asdfasdasd", [filter02.build(), filter03.build()]))
+print(msg_req.sendMessage("wss://relay.damus.io", "asdfasdasd", [filter03.build()]))
+
+
+msg_close = message.MessageClose()
+print(msg_close.sendMessage(relay="wss://relay.damus.io", subscription="asdfasdasd"))
 
