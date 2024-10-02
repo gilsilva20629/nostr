9cd5d6eaad620d0e41d628294b24356e696de9bc
Gilberto <gilsilva20629@gmail.com>
2024-10-02 00:03:47 -0300

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,70 +2,77 @@ from time import time
 import json
 
 class Filter:
+	DAY=86400
+	WEEK=DAY*7
+	MONTH=DAY*30
+	YEAR=MONTH*12
 	
 	def __init__(
 		self,
-		ids: list,
 		authors: list,
 		kinds: list,
-		letter: str,
-		letter_list: list,
-		since: int,
-		until: int,
-		limit: int
+		limit: int,
+		ids=None,
+		letter=None,
+		letter_list=None,
+		since=None,
+		until=None
 		):
 
-		self.ids = ids
 		self.authors = authors
 		self.kinds = kinds
+		self.limit = limit
+		self.ids = ids
 		self.letter = letter
 		self.letter_list = letter_list
 		self.since = since
 		self.until = until
-		self.limit = limit
+		
+		self.args_list = [
+			self.authors,
+			self.kinds,
+			self.limit,
+			self.ids,
+			self.letter,
+			self.letter_list,
+			self.since,
+			self.until
+		]
 
-	def __init__(
-		self,
-		authors: list,
-		kinds: list,
-		limit: int
-		):
-
-		self.ids = None
-		self.authors = authors
-		self.kinds = kinds
-		self.letter = None
-		self.letter_list = None
-		self.since = None
-		self.until = None
-		self.limit = limit
+		self.local = locals()
 
 	def build(self):
-		if self.ids == None:
+		ready_filter = {}
+		for arg in self.args_list:
+			#print("arg:", arg)
+			if arg != None:
+				for x, y in self.local.items():
+					#print("x, y:", x,"_", y)
+					if arg == y :
+						if isinstance(arg, str) and len(arg) == 1 and arg in "pe" :
+							'''
+							try:
+								print(len(arg), type(arg))
+							except Exception as E:
+								print("ocoreu um erro!", E)
+							'''
+							ready_filter.update({"#" + arg: self.letter_list })
 
-			ready_filter = {
-				"authors": self.authors,
-				"kinds": self.kinds,
-				"limit": self.limit
-			}
-			#return json.dumps(ready_filter)
-			return ready_filter
+						else:
+							'''
+							try:
+								print(len(arg), type(arg))
+								#ready_filter[x] = arg
+							except Exception as E:
+								print("ocoreu um erro!", E)
+							'''
+							ready_filter.update({x: arg})
 
-		else:
+		if "letter_list" in ready_filter :
+			ready_filter.pop("letter_list")
 
-			ready_filter = {
-				"ids": self.ids,
-				"authors": self.authors,
-				"kinds": self.kinds,
-				f"#{self.letter}": self.letter_list,
-				"since": self.since,
-				"until": self.until,
-				"limit": self.limit
-			}
-			#return json.dumps(ready_filter)
-			return ready_filter
-
+		#print("Teste de build:", ready_filter, end="\n\n")
+		return ready_filter
 		
 		
-
-		
+				
